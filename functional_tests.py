d227c68b680572a3b8dc03bf5c0531907627ac1e
David Deering <daviddeering3@gmail.com>
2015-06-03 15:59:06 -0500

    Changed functional_tests

--- functional_tests.py
+++ functional_tests.py
@@ -1,6 +1,30 @@
 from selenium import webdriver
+import unittest
 
-browser = webdriver.Firefox()
-browser.get('http://localhost:8000')
+class NewVisitorTest(unittest.TestCase):
+	def setUp(self):
+		self.browser = webdriver.Firefox()
+		self.browser.implicitly_wait(5)
+	
+	def tearDown(self):
+		self.browser.quit()
+		
+	def test_can_start_a_list_and_retrieve_it_later(self):
+		
+#go to the homepage
+		self.browser.get('http://localhost:8000')
+#put in a title and header mentioning to do lists
+		self.assertIn( 'To-Do', self.browser.title)
+		self.fail('Finish the test!')
+		
 
-assert 'Django' in browser.title
+#invite to enter an item
+#enter an item
+#when enter is pushed, page updates showing the list
+#still a text box for another entry
+#do another entry
+#page updates after enter
+#site generates a unique url to save list, explains that
+#url still has the list
+if __name__ == '__main__':
+	unittest.main(warnings='ignore')
