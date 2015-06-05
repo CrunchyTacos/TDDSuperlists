a5a0d86858bdf7c7d9e39a5d5e8410c44c2e01af
David Deering <daviddeering3@gmail.com>
2015-06-05 16:39:18 -0500

    ABout to refactor

--- functional_tests.py
+++ functional_tests.py
@@ -10,6 +10,11 @@ class NewVisitorTest(unittest.TestCase):
 	def tearDown(self):
 		self.browser.quit()
 		
+	def check_for_row_in_list_table:
+		table = self.browser.find_element_by_id('id_list_table')
+		rows = table.find_elements_by_tag_name('tr')
+		self.assertIn(row_text, [row.text for row in rows])
+		
 	def test_can_start_a_list_and_retrieve_it_later(self):
 		
 #go to the homepage
@@ -35,18 +40,16 @@ class NewVisitorTest(unittest.TestCase):
 #when enter is pushed, page updates showing the list
 		inputbox.send_keys(Keys.ENTER)
 		
-		table = self.browser.find_element_by_id('id_list_table')
-		rows = table.find_elements_by_tag_name('tr')
-		self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
+		self.check_for_row_in_list_table('1: Buy peacock feathers')
 #still a text box for another entry, do another entry
 		inputbox = self.browser.find_element_by_id('id_new_item')
 		inputbox.send_keys('Use peacock feathers to make a fly')
 		inputbox.send_keys(Keys.ENTER)
 #page updates after enter
-		table = self.browser.find_element_by_id('id_list_table')
-		rows = table.find_elements_by_tag_name('tr')
-		self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
-		self.assertIn('2: Use peacock feathers to make a fly', [row.text for row in rows])
+		self.check_for_row_in_list_table('1: Buy peacock feathers')
+		self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
+		
+		
 		self.fail('Finish the test')
 #site generates a unique url to save list, explains that
 #url still has the list
