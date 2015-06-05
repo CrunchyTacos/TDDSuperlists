8eeb675f016d9689d491e93372cf76882e4c0987
David Deering <daviddeering3@gmail.com>
2015-06-05 14:09:02 -0500

    Functional test now checks we can input a to-do item

--- functional_tests.py
+++ functional_tests.py
@@ -1,10 +1,11 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 import unittest
 
 class NewVisitorTest(unittest.TestCase):
 	def setUp(self):
 		self.browser = webdriver.Firefox()
-		self.browser.implicitly_wait(5)
+		self.browser.implicitly_wait(3)
 	
 	def tearDown(self):
 		self.browser.quit()
@@ -15,13 +16,31 @@ class NewVisitorTest(unittest.TestCase):
 		self.browser.get('http://localhost:8000')
 #put in a title and header mentioning to do lists
 		self.assertIn( 'To-Do', self.browser.title)
-		self.fail('Finish the test!')
+		header_text = self.browser.find_element_by_tag_name('h1').text
+		self.assertIn('To-Do, header_text')
+		
 		
 
 #invite to enter an item
+		inputbox = self.browser.find_element_by_id('id_new_item')
+		self.assertEqual(
+			inputbox.get_attribute('placeholder'), 
+			'Enter a to-do item')
+			
+		
 #enter an item
+		inputbox.send_keys('Buy peacock feather')
+		
+		
 #when enter is pushed, page updates showing the list
+		inputbox.send_keys(Keys.ENTER)
+		
+		table = self.browser.find_element_by_id('id_list_table')
+		rows = table.find_elements_by_tag_name('tr')
+		self.assertTrue(
+			any(row.text == '1: Buy peacock feathers' for row in rows))
 #still a text box for another entry
+		self.fail('Finish the test')
 #do another entry
 #page updates after enter
 #site generates a unique url to save list, explains that
